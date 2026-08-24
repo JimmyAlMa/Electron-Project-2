de6c2fe22a3d32e9be9f2c55234530c6202df295
Jimmy Al Ma <jimmyalma2121@gmail.com>
2026-08-24 18:22:47 +0700

    feat: expose getStock function in main world

--- preload.js
+++ preload.js
@@ -1,5 +1,6 @@
 const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('stockApi', {
-    addStock: (name, price, total) => ipcRenderer.invoke('add-stock', name, price, total)
+    addStock: (name, price, total) => ipcRenderer.invoke('add-stock', name, price, total),
+    getStock: () => ipcRenderer.invoke('get-stock')
 })
