5a79d00b528a52fcaa80b982084407dd5a3013b2
Jimmy Al Ma <jimmyalma2121@gmail.com>
2026-08-24 19:00:48 +0700

    feat: expose showErrorMessage API via contextBridge in preload

--- preload.js
+++ preload.js
@@ -4,3 +4,7 @@ contextBridge.exposeInMainWorld('stockApi', {
     addStock: (name, price, total) => ipcRenderer.invoke('add-stock', name, price, total),
     getStock: () => ipcRenderer.invoke('get-stock')
 })
+
+contextBridge.exposeInMainWorld('dialogApi', {
+    showErrorMessage: (message) => ipcRenderer.invoke('show-error', message)
+})
