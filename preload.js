fc774eee93199ccf2a5086016b959c0012bc6d19
Jimmy Al Ma <jimmyalma2121@gmail.com>
2026-08-26 18:06:12 +0700

    feat: expose update-total-stock in main world

--- preload.js
+++ preload.js
@@ -3,7 +3,8 @@ const { contextBridge, ipcRenderer } = require('electron')
 contextBridge.exposeInMainWorld('stockApi', {
     addStock: (name, price, total) => ipcRenderer.invoke('add-stock', name, price, total),
     getStock: () => ipcRenderer.invoke('get-stock'),
-    deleteStock: (id) => ipcRenderer.invoke('delete-stock', id)
+    deleteStock: (id) => ipcRenderer.invoke('delete-stock', id),
+    updateTotalStock: (qty, id) => ipcRenderer.invoke('update-total-stock', qty, id)
 })
 
 contextBridge.exposeInMainWorld('dialogApi', {
