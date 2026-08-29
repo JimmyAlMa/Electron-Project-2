45a233eb9ac0d5a2d77e2a0a4bc875f112d613e2
Jimmy Al Ma <jimmyalma2121@gmail.com>
2026-08-29 18:02:31 +0700

    feat: expose checkout-stock in main world as checkoutCart

--- preload.js
+++ preload.js
@@ -4,7 +4,8 @@ contextBridge.exposeInMainWorld('stockApi', {
     addStock: (name, price, total) => ipcRenderer.invoke('add-stock', name, price, total),
     getStock: () => ipcRenderer.invoke('get-stock'),
     deleteStock: (id) => ipcRenderer.invoke('delete-stock', id),
-    updateTotalStock: (qty, id) => ipcRenderer.invoke('update-total-stock', qty, id)
+    updateTotalStock: (qty, id) => ipcRenderer.invoke('update-total-stock', qty, id),
+    checkoutCart: (cartItem) => ipcRenderer.invoke('checkout-stock', cartItem)
 })
 
 contextBridge.exposeInMainWorld('dialogApi', {
